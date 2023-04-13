f33c027e6c880495da8b2cde10738519e4fca75a
jesus2224849 <94402397+jesus2224849@users.noreply.github.com>
2023-04-12 23:54:57 -0600

    actividad opcional

---
+++ cliente/clase-12-angular-inventario-uia-examen-master/src/app/consulta-solicitud-entregada/consulta-solicitud-entregada.component.ts
@@ -0,0 +1,91 @@
+import { Component, ViewChild } from '@angular/core';
+import { ISolicitudMatEntregado } from '../ISolicitudMaterial';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { Router } from '@angular/router';
+import { DatosService } from '../datos.service';
+import { ModalDetailsComponent } from '../modal-details/modal-details.component';
+import { ModalEditComponent } from '../modal-edit/modal-edit.component';
+import { ModalPartidasComponent } from '../modal-partidas/modal-partidas.component';
+
+
+@Component({
+  selector: 'app-consulta-solicitud-entregada',
+  templateUrl: './consulta-solicitud-entregada.component.html',
+  styleUrls: ['./consulta-solicitud-entregada.component.css']
+})
+export class ConsultaSolicitudEntregadaComponent {
+
+  solicitudesentregadas: ISolicitudMatEntregado[] = [];
+
+  pageActual:number=1;
+  urlaccion:string = 'assets/icon-tabla/';
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  //Dynamic Data Variable
+  data: any;
+  public totalRows = 0;
+
+  selectedSolicitud?: ISolicitudMatEntregado;
+ 
+  constructor(public dialog: MatDialog, public datosSolicitud:DatosService) 
+  {     
+
+  }
+
+
+  ngOnInit(): void {
+                
+    this.datosSolicitud.getSolicitudesMaterial().subscribe((data: ISolicitudMatEntregado[])=>{
+      console.log(data);
+      this.solicitudesentregadas = data;
+    })    
+  }   
+
+ 
+ 
+ 
+  openDialogEdit() {
+    const dialogRef = this.dialog.open(ModalEditComponent);
+  
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  
+  openDialogNew() {
+    const dialogRef = this.dialog.open(ModalPartidasComponent);
+  
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  
+  openDialogDelete(id:string) {
+    console.log("id:"+id);
+    this.datosSolicitud.setIdSolicitud(id);
+    const dialogRef = this.dialog.open(ModalEditComponent);
+  
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  
+  
+  openDialogDetails(id:string) {
+    console.log("id:"+id);
+    this.datosSolicitud.setIdSolicitud(id);
+    const dialogRef = this.dialog.open(ModalDetailsComponent);
+  
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  
+
+}
+
+
+
+
+       
+  
